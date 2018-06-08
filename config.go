18ec9292e1454bbc797b27b216ce78c133d140a0
Alexandru Pomohaci <alexandrupomohaci@yahoo.com>
2018-06-08 19:25:34 +0300

    Further required modifications completed

--- config.go
+++ config.go
@@ -3,7 +3,7 @@ package main
 import "github.com/kelseyhightower/envconfig"
 
 type ConfigurationSpec struct {
-	Port int    `default:"8000"`
+	Port int    `default:"7310"`
 	Path string `default:"example-course"`
 }
 
